bd6ee9c97157b75d0591a5429a89b3a72ecd1141
Danila Rapotkin <daysprng@gmail.com>
2024-09-12 13:02:19 +0300

    comments

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,19 @@ const images = [
   },
   {
     src: "https://wowlife-crm.ru/upload/iblock/255/sv29g00fq0e870omdicmh55cllruvqwh/adrenaline-motocross-rider-in-action-2022-09-29-20-31-08-utc.jpg",
+    comment: "",
   },
   {
     src: "https://msk.wowlife.club/_next/image/?url=%2Fimages%2Fakcia-4.jpeg&w=828&q=100",
+    comment: "",
   },
   {
     src: "http://static.kremlin.ru/media/events/photos/medium/kSgH0dqfDDsDZf4t0dBARhBG5BW1EKl2.jpg",
+    comment: "",
   },
   {
     src: "https://wowlife-crm.ru/upload/iblock/bf3/a1mmapopt5jf1lgvhvwo92y44t9ztsdq/minimalist-image-of-colorful-hot-air-balloon-again-2022-11-07-07-22-51-utc.jpg",
+    comment: "",
   },
 ];
 
