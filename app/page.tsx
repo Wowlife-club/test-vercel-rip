b0094e7ea07e3790b247817d4cf6e3db09f7483c
Danila Rapotkin <daysprng@gmail.com>
2024-09-12 12:55:23 +0300

    add img

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 const images = [
   {
     src: "https://wowlife.club/images/banners/category/den-rozhdeniya.jpg",
+    comment: "",
   },
   {
     src: "https://wowlife-crm.ru/upload/iblock/255/sv29g00fq0e870omdicmh55cllruvqwh/adrenaline-motocross-rider-in-action-2022-09-29-20-31-08-utc.jpg",
@@ -12,6 +13,9 @@ const images = [
   },
   {
     src: "http://static.kremlin.ru/media/events/photos/medium/kSgH0dqfDDsDZf4t0dBARhBG5BW1EKl2.jpg",
+  },
+  {
+    src: "https://wowlife-crm.ru/upload/iblock/bf3/a1mmapopt5jf1lgvhvwo92y44t9ztsdq/minimalist-image-of-colorful-hot-air-balloon-again-2022-11-07-07-22-51-utc.jpg",
   },
 ];
 
@@ -28,7 +32,7 @@ export default function Home() {
               fill
             />
             <div className="absolute left-5 right-5 bottom-5 text-lg text-black bg-white p-5 rounded-lg">
-              {img.src}
+              {img.src + " " + img.comment}
             </div>
           </div>
         ))}
